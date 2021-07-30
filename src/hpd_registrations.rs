59b71e2f2c72fa7becf5af94c9311c376bbec696
Atul Varma <varmaa@gmail.com>
2021-07-30 08:58:43 -0400

    Parse BBL and reg end date.

--- src/hpd_registrations.rs
+++ src/hpd_registrations.rs
@@ -1,28 +1,43 @@
 use serde::Deserialize;
-use std::collections::HashMap;
 use std::error::Error;
+use chrono::NaiveDate;
 
 #[derive(Debug, Deserialize)]
-struct HpdRegistration {
+struct RawHpdRegistration {
     #[serde(alias = "RegistrationID")]
     reg_id: u32,
 
     #[serde(alias = "BoroID")]
     boro: u8,
+
+    #[serde(alias = "Block")]
+    block: u32,
+
+    #[serde(alias = "Lot")]
+    lot: u16,
+
+    #[serde(alias = "RegistrationEndDate")]
+    reg_end_date: String,
 }
 
 pub struct HpdRegistrationMap {
-    registrations: HashMap<u32, HpdRegistration>,
 }
 
 impl HpdRegistrationMap {
     pub fn from_csv<T: std::io::Read>(mut rdr: csv::Reader<T>) -> Result<Self, Box<dyn Error>> {
-        let registrations = HashMap::new();
+        let mut count = 0;
 
         for result in rdr.deserialize() {
-            let _record: HpdRegistration = result?;
+            let r: RawHpdRegistration = result?;
+            let reg_end_date = NaiveDate::parse_from_str(&r.reg_end_date.as_ref(), "%m/%d/%Y");
+            if let Err(err) = reg_end_date {
+                println!("Unable to parse registration end date: {}", err);
+            }
+            count += 1;
         }
 
-        Ok(HpdRegistrationMap { registrations })
+        println!("Parsed {} registrations.", count);
+
+        Ok(HpdRegistrationMap {})
     }
 }
