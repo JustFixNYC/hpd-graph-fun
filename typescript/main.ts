340a3bb5348a98753e20a138e00b02ecb9901823
Atul Varma <varmaa@gmail.com>
2021-08-04 07:35:22 -0400

    Add search form, fork typings.

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,5 +1,5 @@
 import { getHTMLElement } from '@justfixnyc/util';
-import ForceGraph, { GraphData } from 'force-graph';
+import ForceGraph, { GraphData, LinkObject, NodeObject } from 'force-graph';
 
 function getEdgeColor(edge: PortfolioEdge): string {
   if (edge.reg_contacts === 1) {
@@ -12,13 +12,13 @@ function getEdgeColor(edge: PortfolioEdge): string {
 
 function portfolioToGraphData(p: Portfolio): GraphData {
   return {
-    nodes: p.nodes.map(node => ({
+    nodes: p.nodes.map((node): NodeObject => ({
       id: node.id,
       name: 'Name' in node.value ? node.value.Name : node.value.BizAddr,
       color: 'Name' in node.value ? 'crimson' : 'gray',
       val: 10,
     })),
-    links: p.edges.map(edge => ({
+    links: p.edges.map((edge): LinkObject => ({
       source: edge.from,
       name: `${edge.reg_contacts} HPD registration${edge.reg_contacts === 1 ? '' : 's'}`,
       color: getEdgeColor(edge),
@@ -29,23 +29,61 @@ function portfolioToGraphData(p: Portfolio): GraphData {
 
 async function main() {
   const graphEl = getHTMLElement('div', '#graph');
+  const messageEl = getHTMLElement('p', '#message');
+  const searchForm = getHTMLElement("form", "#search-form");
+  const searchInput = getHTMLElement("input", "#search-input");
 
   const filename = 'portfolio.json';
   const portfolioReq = await fetch(filename);
 
+  searchInput.value = "";
+
   if (!portfolioReq.ok) {
-    alert(`Got HTTP ${portfolioReq.status} when trying to retrive ${filename}`);
+    messageEl.textContent = `Got HTTP ${portfolioReq.status} when trying to retrive ${filename}!`;
     return;
   }
 
   const portfolio: Portfolio = await portfolioReq.json();
+  const selectedNodes = new Set<NodeObject>();
 
   document.title = portfolio.title;
   getHTMLElement('h1', '').textContent = document.title;
 
   const graphData = portfolioToGraphData(portfolio);
 
-  const graph = ForceGraph()(graphEl).linkDirectionalParticles(2).graphData(graphData);
+  const graph = ForceGraph()(graphEl).linkDirectionalParticles(2).graphData(graphData).nodeColor(node => {
+    return selectedNodes.has(node) ? "blue" : node.color;
+  });
+
+  searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const query = searchInput.value.toUpperCase();
+
+    selectedNodes.clear();
+
+    if (!query) {
+      messageEl.textContent = "";
+      return;
+    }
+
+    for (let node of graphData.nodes) {
+      if (node.name.includes(query)) {
+        selectedNodes.add(node);
+      }
+    }
+
+    if (selectedNodes.size > 0) {
+      messageEl.textContent = `Found ${selectedNodes.size} node(s) with the text "${query}".`;
+      if (selectedNodes.size === 1) {
+        const bbox = graph.getGraphBbox(node => selectedNodes.has(node));
+        graph.centerAt(bbox.x[0], bbox.y[0], 500);
+      } else {
+        graph.zoomToFit(500, 20, node => selectedNodes.has(node));
+      }
+    } else {
+      messageEl.textContent = `Unable to find any nodes with the text "${query}".`;
+    }
+  });
 }
 
 window.addEventListener("load", main);
