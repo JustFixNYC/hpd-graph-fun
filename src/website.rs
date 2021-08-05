3f1a3cb858c373f05526d4bdc130f0fa86c6dee3
Atul Varma <varmaa@gmail.com>
2021-08-05 07:10:51 -0400

    Add maud, use it a bit.

--- src/website.rs
+++ src/website.rs
@@ -1,8 +1,36 @@
+use maud::{html, DOCTYPE};
+use std::rc::Rc;
+
 use super::hpd_registrations::HpdRegistrationMap;
-use super::portfolio::PortfolioMap;
+use super::portfolio::{Portfolio, PortfolioMap};
+
+fn write_portfolio_html(portfolio: &Rc<Portfolio>) {
+    let page = html! {
+        (DOCTYPE)
+        meta charset="utf-8";
+        link rel="stylesheet" href="styles.css";
+        title { (portfolio.name()) }
+        div id="graph" {}
+        h1 { (portfolio.name()) }
+        form id="search-form" {
+            input type="search" value="" placeholder="🔎" id="search-input";
+            button type="submit" { "Search" }
+        }
+        p id="message" {}
+        script src="main.bundle.js" { }
+    };
+
+    let html = page.into_string();
+
+    println!("TODO: Write {}", html);
+}
 
 pub fn make_website(portfolio_map: PortfolioMap, regs: &HpdRegistrationMap, min_buildings: usize) {
     let portfolios = portfolio_map.rank_by_building_count(&regs, min_buildings);
 
-    println!("TODO: Export {} portfolios.", portfolios.len());
+    for (portfolio, _) in &portfolios {
+        write_portfolio_html(portfolio);
+    }
+
+    println!("Exported {} portfolios.", portfolios.len());
 }
